b7aff20fc6978cbed57273fd1601efb6b098a366
KevinMasci <kevin-sor@hotmail.com>
2023-04-14 22:15:47 -0300

    Lab01

---
+++ Unidad02/Lab01/Decision/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese texto");
+        string imputTexto = Console.ReadLine();
+        if (!(imputTexto.Any(char.IsLetter)))
+        {
+            Console.WriteLine("El texto ingresado contiene numeros");
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("1. Mayuscula");
+            Console.WriteLine("2. Minuscula");
+            Console.WriteLine("3. Cantidad de caracteres");
+            Console.WriteLine("Elija una opcion");
+            ConsoleKeyInfo opcion = Console.ReadKey();
+
+            if (opcion.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine(imputTexto.ToUpper());
+            }
+            else if (opcion.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine(imputTexto.ToLower());
+            }
+            else if (opcion.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine(imputTexto.Length);
+            }
+            else
+            {
+                Console.WriteLine("No se eligio ninguna opcion");
+            }
+
+            /*bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("1. Mayuscula");
+                Console.WriteLine("2. Minuscula");
+                Console.WriteLine("3. Cantidad de caracteres");
+                Console.WriteLine("Elija una opcion");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+
+                switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine(imputTexto.ToUpper());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine(imputTexto.ToLower());
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine(imputTexto.Length);
+                        break;
+                    default:
+                        salir = true;
+                        break;
+                }
+            }*/
+        }
+    }
+}
