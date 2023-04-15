9927e9b36e10bbb7feb54f33be98333746c07df1
KevinMasci <kevin-sor@hotmail.com>
2023-04-14 22:38:23 -0300

    Lab01

--- Unidad02/Lab01/Iteracion/Program.cs
+++ Unidad02/Lab01/Iteracion/Program.cs
@@ -2,6 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        string[] arreglo;
+        int cantIteraciones = 5;
+        string[] arreglo = new string[cantIteraciones];
+
+        for (int i = 0; i < cantIteraciones; i++)
+        {
+            Console.WriteLine("Ingrese el valor de la posicion {0}", i);
+            arreglo[i] = Console.ReadLine();
+        }
+        for (int i = arreglo.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(arreglo[i]);
+        }
+        Console.ReadKey();
     }
 }
