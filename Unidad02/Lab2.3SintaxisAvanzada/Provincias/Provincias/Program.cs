fcbb248ab44fedff3a1a27f9492831a8b2b5d5b7
KevinMasci <kevin-sor@hotmail.com>
2023-07-02 19:45:38 -0300

    terminado ej 1

---
+++ Unidad02/Lab2.3SintaxisAvanzada/Provincias/Provincias/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] provincias = {
+        "Buenos Aires",
+        "Catamarca",
+        "Chaco",
+        "Chubut",
+        "Córdoba",
+        "Corrientes",
+        "Entre Ríos",
+        "Formosa",
+        "Jujuy",
+        "La Pampa",
+        "La Rioja",
+        "Mendoza",
+        "Misiones",
+        "Neuquén",
+        "Río Negro",
+        "Salta",
+        "San Juan",
+        "San Luis",
+        "Santa Cruz",
+        "Santa Fe",
+        "Santiago del Estero",
+        "Tierra del Fuego",
+        "Tucumán"
+        };
+
+        var privinciasFiltradas = provincias.Where(p => p.StartsWith('S') || p.StartsWith('T'));
+
+        foreach (var provincia in privinciasFiltradas)
+        {
+            Console.WriteLine(provincia);
+        }
+    }
+}
