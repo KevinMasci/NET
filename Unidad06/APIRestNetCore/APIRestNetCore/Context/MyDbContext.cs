59bd83c3639a535719084f6136f39091c7f28057
KevinMasci <kevin-sor@hotmail.com>
2023-10-13 12:53:54 -0300

    APIRestNetCore

---
+++ Unidad06/APIRestNetCore/APIRestNetCore/Context/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIRestNetCore.Models;
+
+namespace APIRestNetCore.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
